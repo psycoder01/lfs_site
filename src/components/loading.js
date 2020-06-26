93e122ce712385c8e92fda9d8231491e3f2ab8ae
psycoder01 <aakashchy13@gmail.com>
2020-06-26 08:52:58 +0545

    entry animation added

--- src/components/loading.js
+++ src/components/loading.js
@@ -4,7 +4,7 @@ const Loading = () => (
   <div
     className={`loading-container mx-auto w-1/2 h-screen flex items-center`}
   >
-    <div className="font-mono flex-1 text-blue-400 text-6xl font-extrabold text-center animate__animated animate__pulse animate__infinite">
+    <div className="font-mono flex-1 text-blue-400 text-6xl font-extrabold text-center pulse">
       LFS
     </div>
   </div>
