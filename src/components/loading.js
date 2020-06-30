697fc8c49a77057c09fc87d503839258a7035d30
psycoder01 <aakashchy13@gmail.com>
2020-06-30 18:48:25 +0545

    added logo in loading screen

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,11 +1,10 @@
 import React from "react";
+import logo from "../img/lfsteam.png";
 
 const Loading = () => (
-  <div
-    className={`loading-container mx-auto w-1/2 h-screen flex items-center`}
-  >
-    <div className="font-mono flex-1 text-blue-400 text-6xl font-extrabold text-center pulse">
-      LFS
+  <div className={`loading-container mx-auto w-1/2 h-screen flex items-center`}>
+    <div className="text-center mx-auto pulse">
+      <img src={logo} alt="loading-logo" className="h-24 sm:h-32 sm:w-80"/>
     </div>
   </div>
 );
